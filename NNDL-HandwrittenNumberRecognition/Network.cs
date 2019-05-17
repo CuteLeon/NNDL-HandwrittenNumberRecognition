7b8c0c0e747b3bda241cb24b3e05d366523aa5f2
Leon <leon.id@qq.com>
2019-05-17 17:47:51 +0800

    增加神经网络类

---
+++ NNDL-HandwrittenNumberRecognition/Network.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NNDL_HandwrittenNumberRecognition
+{
+    /// <summary>
+    /// 神经网络
+    /// </summary>
+    public class Network
+    {
+        public Network()
+        {
+        }
+    }
+}
