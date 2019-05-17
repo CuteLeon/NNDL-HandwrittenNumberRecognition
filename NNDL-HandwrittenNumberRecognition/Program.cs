8a10c6b3d498a6c872b2283afe6e0b398128e64c
Leon <leon.id@qq.com>
2019-05-17 10:14:35 +0800

    fixed;

--- NNDL-HandwrittenNumberRecognition/Program.cs
+++ NNDL-HandwrittenNumberRecognition/Program.cs
@@ -6,7 +6,10 @@ namespace NNDL_HandwrittenNumberRecognition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"神经网络和深度学习-手写数字识别：
+使用三层神经网络结构，28*28=784个输入神经元，15个 隐藏层神经元、10个输出神经元。");
+
+            Console.Read();
         }
     }
 }
