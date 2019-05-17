46edb51f387dbb721ebdf27f6e96d6ff64e3fb86
Leon <leon.id@qq.com>
2019-05-17 10:38:04 +0800

    增加训练和测试数据集；引入 Accord.IO.IdxReader 以解析 IDX 文件；

--- NNDL-HandwrittenNumberRecognition/Program.cs
+++ NNDL-HandwrittenNumberRecognition/Program.cs
@@ -7,7 +7,8 @@ namespace NNDL_HandwrittenNumberRecognition
         static void Main(string[] args)
         {
             Console.WriteLine(@"神经网络和深度学习-手写数字识别：
-使用三层神经网络结构，28*28=784个输入神经元，15个 隐藏层神经元、10个输出神经元。");
+使用三层神经网络结构，28*28=784个输入神经元，15个 隐藏层神经元、10个输出神经元。
+输入为：0~1的实数代表每个像素的灰度值，0.0=白色，1.0=黑色。");
 
             Console.Read();
         }
