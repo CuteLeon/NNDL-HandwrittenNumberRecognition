0616c11ce46e2db115e1f18f7c7e4a54a5fcc37a
Leon <leon.id@qq.com>
2019-05-17 10:09:45 +0800

    添加项目文件。

---
+++ NNDL-HandwrittenNumberRecognition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NNDL_HandwrittenNumberRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
