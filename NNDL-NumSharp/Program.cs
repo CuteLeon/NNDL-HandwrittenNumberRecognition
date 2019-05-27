e5ad4940bf220ec6387955b8c6c677587def1969
Leon <leon.id@qq.com>
2019-05-27 11:39:53 +0800

    增加 NNDL-NumSharp;

---
+++ NNDL-NumSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NNDL.Util;
+
+namespace NNDL_NumSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper.PrintLine("NNDL-NumSharp");
+        }
+    }
+}
