356653c3214997c32c31a8ab1b9929464b5af753
Leon <leon.id@qq.com>
2019-05-27 14:26:39 +0800

    初始化神经网络偏差和权重；

--- NNDL-NumSharp/Network.cs
+++ NNDL-NumSharp/Network.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using NumSharp;
 
 namespace NNDL_NumSharp
 {
@@ -10,12 +12,38 @@ namespace NNDL_NumSharp
     public class Network
     {
         /// <summary>
-        /// 神经元数量
+        /// 神经网络层数
         /// </summary>
-        /// <param name="neurons"></param>
-        public Network(int[] neurons)
-        {
+        public int LayersCount { get; }
 
+        /// <summary>
+        /// 神经网络每一层神经元数量
+        /// </summary>
+        public int[] NeuronsCounts { get; }
+
+        /// <summary>
+        /// 神经网络偏置数组
+        /// </summary>
+        public NDArray[] Biases { get; }
+
+        /// <summary>
+        /// 神经网络权重
+        /// </summary>
+        public NDArray[] Weights { get; }
+
+        /// <summary>
+        /// 神经网络
+        /// </summary>
+        /// <param name="neuronsCounts">神经网络每一层神经元数量</param>
+        public Network(int[] neuronsCounts)
+        {
+            this.LayersCount = neuronsCounts.Length;
+            this.NeuronsCounts = neuronsCounts;
+
+            // 高斯(正态)分布 随机数发生器
+            var pyRandom = new NumPyRandom();
+            this.Biases = neuronsCounts.Skip(1).Select(count => pyRandom.randn(count, 1)).ToArray();
+            this.Weights = neuronsCounts.Skip(1).Zip(neuronsCounts.SkipLast(1)).Select(counts => pyRandom.randn(counts.First, counts.Second)).ToArray();
         }
     }
 }
--- NNDL-NumSharp/Program.cs
+++ NNDL-NumSharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using NNDL.DataReader;
 using NNDL.Util;
 
@@ -6,12 +9,73 @@ namespace NNDL_NumSharp
 {
     class Program
     {
+        static readonly string DataDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Datas");
+        static readonly string TrainImagesPath = Path.Combine(DataDirectory, "train-images.idx3-ubyte");
+        static readonly string TrainLabelsPath = Path.Combine(DataDirectory, "train-labels.idx1-ubyte");
+        static readonly string T10kImagesPath = Path.Combine(DataDirectory, "t10k-images.idx3-ubyte");
+        static readonly string T10kLabelsPath = Path.Combine(DataDirectory, "t10k-labels.idx1-ubyte");
+
         static void Main(string[] args)
         {
             Helper.PrintLine("NNDL-NumSharp");
+            Helper.PrintLine("检查数据文件...");
+            if (!CheckDataFiles()) Exit(-1);
 
+            Helper.PrintLine("读取训练数据...");
             MNISTReader reader = new MNISTReader();
+            var trainImages = reader.ReadMatrixsFlattened(TrainImagesPath).ToArray();
+            var trainLabels = reader.ReadValues(TrainLabelsPath).Select(value => { var values = Enumerable.Repeat<double>(0.0, 10).Cast<double>().ToArray(); values[value] = 1; return values; }).ToArray();
 
+            Helper.PrintLine("创建神经网络...");
+            var network = new Network(new[] { 784, 30, 10 });
+
+            Stopwatch stopwatch = new Stopwatch();
+            Helper.PrintLine("随机梯度下降算法训练神经网络...");
+            Helper.PrintSplit();
+            stopwatch.Start();
+
+            /*
+            network.StochasticGradientDescent(
+                trainImages.Zip(trainLabels),
+                3,
+                10,
+                3.0);
+             */
+
+            stopwatch.Stop();
+            Helper.PrintSplit();
+            Helper.PrintLine($"神经网络训练结束，耗时：{stopwatch.Elapsed.ToString()}");
+
+            Exit();
+        }
+
+        static void Exit(int code = 0)
+        {
+            Helper.PrintLine("程序即将退出...");
+            Console.Read();
+            Environment.Exit(code);
+        }
+
+        static bool CheckDataFiles()
+        {
+            var filesNotExist = new[]
+            {
+                TrainImagesPath,
+                TrainLabelsPath,
+                T10kImagesPath,
+                T10kLabelsPath,
+            }.Where(path => !File.Exists(path))
+            .Select(path => Path.GetFileName(path))
+            .ToArray();
+            if (filesNotExist.Length == 0)
+            {
+                return true;
+            }
+            else
+            {
+                Helper.PrintLine($"缺失文件：\n\t{string.Join("\n\t", filesNotExist)}");
+                return false;
+            }
         }
     }
 }
