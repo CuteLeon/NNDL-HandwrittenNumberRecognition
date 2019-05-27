d19878c45c95afd1e9a553ab0a914d123acb9e77
Leon <leon.id@qq.com>
2019-05-27 16:20:22 +0800

    使用 NumSharp 完成神经网络；

--- NNDL-NumSharp/Network.cs
+++ NNDL-NumSharp/Network.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NNDL.Util;
 using NumSharp;
 
 namespace NNDL_NumSharp
@@ -24,12 +25,12 @@ namespace NNDL_NumSharp
         /// <summary>
         /// 神经网络偏置数组
         /// </summary>
-        public NDArray[] Biases { get; }
+        public NDArray[] Biases { get; protected set; }
 
         /// <summary>
         /// 神经网络权重
         /// </summary>
-        public NDArray[] Weights { get; }
+        public NDArray[] Weights { get; protected set; }
 
         /// <summary>
         /// 神经网络
@@ -44,6 +45,146 @@ namespace NNDL_NumSharp
             var pyRandom = new NumPyRandom();
             this.Biases = neuronsCounts.Skip(1).Select(count => pyRandom.randn(count, 1)).ToArray();
             this.Weights = neuronsCounts.Skip(1).Zip(neuronsCounts.SkipLast(1)).Select(counts => pyRandom.randn(counts.First, counts.Second)).ToArray();
+        }
+
+        /// <summary>
+        /// 随机梯度下降算法训练
+        /// </summary>
+        /// <param name="trainDatas">训练数据</param>
+        /// <param name="epochs">训练纪元数</param>
+        /// <param name="batchSize">批量数据容量</param>
+        /// <param name="eta">学习率</param>
+        public void StochasticGradientDescent(IEnumerable<(NDArray Image, NDArray Label)> trainDatas, int epochs, int batchSize, double eta)
+        {
+            Helper.PrintLine($"训练数据数量：{trainDatas.Count()}，训练纪元：{epochs}，Mini批量数量：{batchSize}，学习率：{eta.ToString("N2")}");
+            Helper.PrintSplit();
+
+            // 使用随机比较器打乱训练数据顺序
+            trainDatas = trainDatas.OrderBy(data => data, new RandomComparer());
+
+            // 遍历训练纪元
+            for (int epoch = 0; epoch < epochs; epoch++)
+            {
+                Helper.PrintLine($"<<< 训练第 {epoch} 纪元 >>>");
+                foreach (var batchs in trainDatas.Splite(batchSize))
+                {
+                    // 使用小批量训练数据更新神经网络的权重和偏置
+                    this.UpdateMiniBatch(batchs, eta);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 使用小批量数据更新网络神经元的偏置和权重
+        /// </summary>
+        /// <param name="trainDatas">小批量训练数据</param>
+        /// <param name="eta">学习率</param>
+        public void UpdateMiniBatch(IEnumerable<(NDArray Image, NDArray Label)> trainDatas, double eta)
+        {
+            NDArray[] nablaBiases = this.Biases.Select(array => np.zeros(array.shape)).ToArray();
+            NDArray[] nablaWeights = this.Weights.Select(array => np.zeros(array.shape)).ToArray();
+
+            // 遍历小批量训练数据
+            foreach (var (Image, Label) in trainDatas)
+            {
+                // 使用反向传播计算偏置和权重的梯度
+                var (deltaNablaBiases, deltaNablaWeight) = this.BackPropogation(Image, Label);
+
+                nablaBiases = nablaBiases.Zip(deltaNablaBiases).Select(b => b.First + b.Second).ToArray();
+                nablaWeights = nablaWeights.Zip(deltaNablaWeight).Select(w => w.First + w.Second).ToArray();
+            }
+
+            // 使用学习率按比例更新 偏置和权重 到神经网络
+            this.Biases = this.Biases.Zip(nablaBiases).Select(b => b.First - (eta / trainDatas.Count() * b.Second)).ToArray();
+            this.Weights = this.Weights.Zip(nablaWeights).Select(w => w.First - (eta / trainDatas.Count()) * w.Second).ToArray();
+        }
+
+        /// <summary>
+        /// 反向传播
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        public (NDArray[] deltaNablaBiases, NDArray[] deltaNablaWeight) BackPropogation(NDArray image, NDArray label)
+        {
+            NDArray[] nablaBiases = this.Biases.Select(array => np.zeros(array.shape)).ToArray();
+            NDArray[] nablaWeights = this.Weights.Select(array => np.zeros(array.shape)).ToArray();
+
+            NDArray activation = image;
+            NDArray z = null, sp = null;
+            List<NDArray> activations = new List<NDArray>() { image };
+            List<NDArray> zs = new List<NDArray>();
+
+            foreach (var (bias, weight) in this.Biases.Zip(this.Weights))
+            {
+                z = np.dot(weight, activation) + bias;
+                zs.Add(z);
+                activation = this.Sigmoid(z);
+                activations.Add(activation);
+            }
+
+            NDArray delta = this.CostDerivative(activations[^1], label) * this.SigmoidPrime(zs[^1]);
+            nablaBiases[^1] = delta;
+            nablaWeights[^1] = np.dot(delta, activations[^2].transpose());
+
+            for (int index = 2; index < this.LayersCount; index++)
+            {
+                z = zs[^index];
+                sp = this.SigmoidPrime(z);
+                delta = np.dot(this.Weights[^(index - 1)].transpose(), delta) * sp;
+                nablaBiases[^index] = delta;
+                nablaWeights[^index] = np.dot(delta, activations[^(index + 1)].transpose());
+            }
+
+            return (nablaBiases, nablaWeights);
+        }
+
+        /// <summary>
+        /// Sigmoid 函数
+        /// </summary>
+        /// <param name="z"></param>
+        /// <returns></returns>
+        /// <remarks>NumSharp 暂未实现 np.exp(ndarray)</remarks>
+        public NDArray Sigmoid(NDArray z)
+            => new NDArray((z.Array as double[]).Select(s => this.Sigmoid(s)).ToArray(), z.shape);
+
+        /// <summary>
+        /// Sigmoid 函数
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public double Sigmoid(double source)
+            => 1.0 / (1.0 + Math.Exp(-source));
+
+        /// <summary>
+        /// Sigmoid 导数
+        /// </summary>
+        /// <param name="sources"></param>
+        /// <returns></returns>
+        public NDArray SigmoidPrime(NDArray sources)
+            => new NDArray((sources.Array as double[]).Select(s => this.Sigmoid(s) * (1 - this.Sigmoid(s))).ToArray(), sources.shape);
+
+        /// <summary>
+        /// 代价函数
+        /// </summary>
+        /// <param name="actuality">真实值</param>
+        /// <param name="expectations">期望值</param>
+        /// <returns></returns>
+        public NDArray CostDerivative(NDArray actuality, NDArray expectations)
+            => actuality - expectations;
+
+        /// <summary>
+        /// 使用神经网络前馈输出
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public NDArray FeedForward(NDArray input)
+        {
+            foreach (var (bias, weight) in this.Biases.Zip(this.Weights))
+            {
+                input = this.Sigmoid(np.dot(weight, input) + bias);
+            }
+            return input;
         }
     }
 }
--- NNDL-NumSharp/Program.cs
+++ NNDL-NumSharp/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using NNDL.DataReader;
 using NNDL.Util;
+using NumSharp;
 
 namespace NNDL_NumSharp
 {
@@ -23,8 +24,8 @@ namespace NNDL_NumSharp
 
             Helper.PrintLine("读取训练数据...");
             MNISTReader reader = new MNISTReader();
-            var trainImages = reader.ReadMatrixsFlattened(TrainImagesPath).ToArray();
-            var trainLabels = reader.ReadValues(TrainLabelsPath).Select(value => { var values = Enumerable.Repeat<double>(0.0, 10).Cast<double>().ToArray(); values[value] = 1; return values; }).ToArray();
+            var trainImages = reader.ReadMatrixsFlattened(TrainImagesPath).Take(1000).Select(array => new NDArray(array, new[] { array.Length, 1 })).ToArray();
+            var trainLabels = reader.ReadValues(TrainLabelsPath).Take(1000).ToArray();
 
             Helper.PrintLine("创建神经网络...");
             var network = new Network(new[] { 784, 30, 10 });
@@ -34,17 +35,21 @@ namespace NNDL_NumSharp
             Helper.PrintSplit();
             stopwatch.Start();
 
-            /*
             network.StochasticGradientDescent(
-                trainImages.Zip(trainLabels),
-                3,
+                trainImages.Zip(trainLabels.Select(value => { var values = Enumerable.Repeat(0.0, 10).Cast<double>().ToArray(); values[value] = 1; return new NDArray(values, new[] { values.Length, 1 }); })),
                 10,
+                20,
                 3.0);
-             */
 
             stopwatch.Stop();
             Helper.PrintSplit();
             Helper.PrintLine($"神经网络训练结束，耗时：{stopwatch.Elapsed.ToString()}");
+
+            Helper.PrintSplit();
+            Helper.PrintLine("预测：");
+            var output = network.FeedForward(trainImages[0]).Array as double[];
+            Helper.PrintLine($"答案：{trainLabels[0]}");
+            Helper.PrintLine($"预测结果：\n\t{string.Join("\n\t", Enumerable.Range(0, output.Length).Select(index => $"{index} => {output[index]:P2}"))}");
 
             Exit();
         }
